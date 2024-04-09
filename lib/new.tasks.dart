2503095e2c9950d4de576d961636a39f21e05e40
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-04-09 20:12:20 -0300

    made the text field and the save button

--- lib/new.tasks.dart
+++ lib/new.tasks.dart
@@ -1,21 +1,45 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class NewTasksPage extends StatelessWidget {
   const NewTasksPage({super.key});
+
+  void _onSaved(BuildContext context) {
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("New Tasks"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.pushReplacementNamed(context,"/tasks");
-            },
-            icon: Icon(Icons.logout),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        child: Container(
+          child: Column(
+            children: [
+              TextField(
+                minLines: 1,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Task",
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                width: double.infinity,
+                child: ElevatedButton(
+                  child: Text("Save"),
+                  onPressed: () => _onSaved(context),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
