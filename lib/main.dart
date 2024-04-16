86d833384b4f26c1effeec23751d03e60cc16bef
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-04-16 16:21:34 -0300

    connect firebase in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
 
+const firebaseConfig = FirebaseOptions(
+    apiKey: "AIzaSyAT_y0lEL5bD0w9enXruKOVv9IrbArFURs",
+    authDomain: "tasklist-9f78e.firebaseapp.com",
+    projectId: "tasklist-9f78e",
+    storageBucket: "tasklist-9f78e.appspot.com",
+    messagingSenderId: "280767043565",
+    appId: "1:280767043565:web:37f1b64331b882f4f479e4");
+
 void main() {
+  Firebase.initializeApp(options: firebaseConfig);
   runApp(const App());
 }
